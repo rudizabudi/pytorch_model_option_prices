9f8813a08795deec6f76f3d47f0faecb53850cc6
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2025-05-08 04:28:51 +0200

    select process by commenting out function

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from sql_request_data import controller
+from train_model import startup
+from config import DataCreationConfig
 
 
 def create_training_data():
     controller()
 
 
+def train_model():
+    startup()
+
+
 if __name__ == "__main__":
     create_training_data()
+    #train_model()
+
+#Todo: Normalize data for stock splits
