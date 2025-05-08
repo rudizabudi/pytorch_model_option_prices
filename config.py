3f7f2e14f83de4fa76902705c49fa60085db9a0d
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2025-05-08 04:26:28 +0200

    financialmodelingprep API for stk dividends added

--- config.py
+++ config.py
@@ -8,12 +8,13 @@ class DataCreationConfig:
         BACKUP = 'BACKUP'
         TRAINING = 'TRAINING'
 
-    OUTPUT_TYPE: OutputType = OutputType.TRAINING  # BACKUP | TRAINING
+    OUTPUT_TYPE: OutputType = OutputType.BACKUP  # BACKUP | TRAINING
     TRAINING_EXPORT_DIR: str = 'training_data'  # directory to save training data
     BACKUP_EXPORT_DIR: str = 'backup_data'  # directory to save backup data
 
     HISTORY_ONLY: bool = True  # only use historical data
     START_DATE: str = '2024-08-01'  # YYYY-MM-DD start date for historical data to process
+    END_DATE_WEEKDAYS: int = 3  # Weekdays before today to cut-off data
     REFRESH_DATA: bool = True  # only process data for tables that dont already exist
 
     SQL_CREDENTIALS_JSON_PATH: str = 'sql_credentials.json'
@@ -22,7 +23,11 @@ class DataCreationConfig:
 
     TRAIN_IGNORE_FOLDERS: str = 'Vergleich'  # comma separated
 
-    FMP_API_KEY: str = 'rbfZHTUCSk52aCXELDmES40UraSXKgJL'  # financialmodelingprep API Key
+    FMP_API_KEY: str = 'rbfZHTUCSk52aCXELDmES40UraSXKgJL,eQeWi3SJy3cfiJxBRN1nuYva71lW2EFU,yauaLdTo5qtEQ2MF9i2jKHFQ9i2gdwG7'  # financialmodelingprep API Key
+    FMP_CHANGE_KEY: int = 240  # change api key after X requests
+
+    # DO NOT CHANGE
+    FMP_API_KEY = tuple(FMP_API_KEY.split(','))
 
 
 @dataclass
@@ -44,5 +49,7 @@ class TrainingConfig:
     TRAINING_EPOCHS: int = 20
 
     MODEL_SAVE_NAME: str = 'full_model.pth'
+
+
 
 
